0b26e4ac1d596dde6c3983a9104c391be1a0a4e4
xXangelXx <angeluchimag17@gmail.com>
2021-03-27 13:42:26 -0400

    create components loading and nav

---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './loading.scss'
+const Loading = (props) => {
+    return(
+        <div className="container-loading">
+
+        </div>
+    )
+}
+export default Loading
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './nav.scss'
+const Nav = (props) => {
+    return(
+        <div className="container-nav">
+
+        </div>
+    )
+}
+export default Nav
