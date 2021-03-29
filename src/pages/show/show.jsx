c5748326de6a9b0b511d187b972bcb3479fbe409
xXangelXx <angeluchimag17@gmail.com>
2021-03-29 16:47:02 -0400

    window.location.href show user

--- src/pages/show/show.jsx
+++ src/pages/show/show.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './show.scss';
 import Nav from '../../components/nav/nav'
 const Show = (props) => {
+    useEffect(()=>{
+        let id = window.location.href? window.location.href.split('/').pop():document.location.href.split('/').pop()
+
+        console.log(id)
+    },[])
     return (
             <div className="container-page-show">
                 <Nav />
