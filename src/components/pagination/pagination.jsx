e80c0bb5b79aca44bf5054d55595b1b244600014
xXangelXx <angeluchimag17@gmail.com>
2021-04-01 15:02:42 -0400

    component pagination

---
+++ src/components/pagination/pagination.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './pagination.scss';
+const Pagination = (props) => {
+    return (
+        <div className="container-pagination">
+
+        </div>
+    )
+}
+
+const MapStateToProps = (state)=> {
+    return{
+
+    }
+}
+const MapDispatchToProps = (dispatch)=> {
+    return {
+
+    }
+}
+export default connect(MapStateToProps,MapDispatchToProps)(Pagination)
