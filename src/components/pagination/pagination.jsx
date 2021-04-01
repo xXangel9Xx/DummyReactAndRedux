dace5187c03935f5a9b6ece550efeb90b925e29f
xXangelXx <angeluchimag17@gmail.com>
2021-04-01 18:06:34 -0400

    improve function change color

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -10,21 +10,18 @@ const Pagination = (props) => {
         <div className="container-pagination">
             <ul className="ul-container">
                 <li className="li-index" id="id-look-out" 
-                value={0}
                 onClick={(e)=>{props.dispatch(props.getIndex(`https://dummyapi.io/data/api/user?page=${props.pagination.positionOne}&limit=10`))
                 changeColorPagination('li-index','id-look-out',0)
             }}>
                     {props.pagination.positionOne}
                     </li>
                 <li className="li-index" 
-                value={1}
                 onClick={(e)=>{props.dispatch(props.getIndex(`https://dummyapi.io/data/api/user?page=${props.pagination.positionTwo}&limit=10`))
                 changeColorPagination('li-index','id-look-out',1)
             }}>
                     {props.pagination.positionTwo}
                 </li>
                 <li className="li-index"
-                value={2}
                 onClick={(e)=>{props.dispatch(props.getIndex(`https://dummyapi.io/data/api/user?page=${props.pagination.positionThree}&limit=10`))
                 changeColorPagination('li-index','id-look-out',2)
             }}> 
--- src/pages/index/utils/changeColorPagination.jsx
+++ src/pages/index/utils/changeColorPagination.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
 export default function changeColorPagination(className,elementID,index){
-
-    if(document.getElementsByClassName(className)[index].getAttribute("id")){
-        
-    }else{
+    if(!document.getElementsByClassName(className)[index].getAttribute("id")){
         document.getElementById(elementID).removeAttribute("id")
-        document.getElementsByClassName(className)[index].setAttribute("id",elementID)
+        document.getElementsByClassName(className)[index].setAttribute("id",elementID)        
     }
-    
-    
 }
