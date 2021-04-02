eb41c1d351329ad4486cfab497bf5ee09bcc91c2
xXangelXx <angeluchimag17@gmail.com>
2021-04-02 17:22:56 -0400

    src/components/pagination/arrowLeft/arrowLeft.jsx decrement ready

--- src/components/pagination/arrowLeft/arrowLeft.jsx
+++ src/components/pagination/arrowLeft/arrowLeft.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './arrowLeft.scss';
+import {setPaginationDecrement} from '../../../redux/action/pagination/paginationAction'
+import { connect } from 'react-redux';
 const ArrowLeft = (props) => {
     function shangeColorIndexPagination(classNameLi,idNameLi){
         let divsLi = document.getElementsByClassName(classNameLi)
         let idLi = document.getElementById(idNameLi)
         let index = idLi.value
+        if(index==0){
+            props.dispatch(props.setPaginationDecrement())
+        }        
+
         if(index>0){
             index-=1
             idLi.removeAttribute("id")
@@ -17,4 +23,15 @@ const ArrowLeft = (props) => {
         </div>
     )
 }
-export default ArrowLeft
+const MapStateToProps = (state) => {
+    return {
+
+    }
+}
+const MapDispatchToProps = (dispatch) => {
+    return {
+        setPaginationDecrement,
+        dispatch
+    }
+}
+export default connect(MapStateToProps,MapDispatchToProps)(ArrowLeft)
