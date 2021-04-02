6409264d380054ec9524095ea3598b155aa8c082
xXangelXx <angeluchimag17@gmail.com>
2021-04-02 13:45:13 -0400

    src/pages/index/utils/changeColorPagination.js

--- src/pages/index/utils/changeColorPagination.js
+++ src/pages/index/utils/changeColorPagination.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function changeColorPagination(className,elementID,index){
     if(!document.getElementsByClassName(className)[index].getAttribute("id")){
         document.getElementById(elementID).removeAttribute("id")
